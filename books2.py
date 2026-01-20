dae3b59d06aedb32301c32c57b9cdf884fb0a661
unknown <aditya.p.gouda2@gmail.com>
2026-01-20 09:31:26 +0530

    1/20/2026

--- books2.py
+++ books2.py
@@ -23,11 +23,22 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description="ID is not needed on create", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=-1, lt=6)
+    
+    model_config = {
+        "json_schema_extra" : {
+            "example" : {
+                "title" : "A new book",
+                "author" : "aditya",
+                "description" : "A new description of a book",
+                "rating" : 5
+            }
+        }
+    }
 
 
 BOOKS = [
@@ -43,6 +54,12 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.get("/books/{book_id}")
+async def read_book(book_id:int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
 
 
 @app.post("/create-book")
